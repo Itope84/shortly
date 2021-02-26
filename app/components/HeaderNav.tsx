26fa721b12f6d546d6ebbc3b35109ff60f460c5b
Temitope <topephysics@gmail.com>
2021-02-26 11:09:50 +0100

    removed default index content

---
+++ app/components/HeaderNav.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Header.module.scss';
+import * as React from 'react'
+// import { IPost } from '../types'
+
+const HeaderNav: React.FC = () => {
+    return <header>Hello</header>
+}
+
+export default HeaderNav;
