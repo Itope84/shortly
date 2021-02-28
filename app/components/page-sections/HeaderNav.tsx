764f1104ab616ab973e3131492b85b9d347c3403
Temitope <topephysics@gmail.com>
2021-02-28 18:36:02 +0100

    implemented mobile view of navbar

--- app/components/page-sections/HeaderNav.tsx
+++ app/components/page-sections/HeaderNav.tsx
@@ -1,13 +1,28 @@
 import styles from '../../styles/components/Header.module.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import LinkButton from '../buttons/LinkButton'
 
 console.log(styles)
 
 const HeaderNav: React.FC = () => {
+  const [showMobileNav, setShowMobileNav] = useState(false)
+
   return (
-    <header className={styles.header}>
+    <header className={[styles.header, showMobileNav ? styles['is-open'] : ''].join(' ')}>
       <img src="/images/logo.svg" alt="Shortly Logo" className={styles.logo} />
+
+      <button
+        className={styles['mobile-trigger']}
+        onClick={(): void => setShowMobileNav(!showMobileNav)}
+      >
+        {/* {Array(3)
+          .fill(null)
+          .map(() => ( */}
+        <span className={styles.dash}></span>
+        <span className={styles.dash}></span>
+        <span className={styles.dash}></span>
+        {/* ))} */}
+      </button>
 
       <nav className={`${styles.nav} ${styles['nav__main']}`}>
         <ul>
